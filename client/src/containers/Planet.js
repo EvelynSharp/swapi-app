d0d23c6779d4af1dac60aff1562f944d5219c9f2
Evelyn Sharp <yihongshi90@gmail.com>
2017-07-27 13:04:35 -0600

    style planet

--- client/src/containers/Planet.js
+++ client/src/containers/Planet.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import { fetchHome } from '../actions/planets';
 import { fetchResidents } from '../actions/people';
 import { connect } from 'react-redux';
+import { List, Grid, Button } from 'semantic-ui-react';
 
 class Planet extends React.Component {
 
@@ -22,24 +23,33 @@ class Planet extends React.Component {
       return planet.residents.map( r => {
         let id = residents[r].url.replace(/^\D+/g, '');
         return (
-          <li key={r} onClick={ () => history.push(`/people/${id}`)}>
-            { residents[r].name }
-          </li>
+          <List.Item key={r} onClick={ () => history.push(`/people/${id}`)}>
+            <List.Icon name='user outline' />
+            <List.Content> { residents[r].name } </List.Content>
+          </List.Item>
         )
       })
     }
   }
 
   render() {
-    let { planet } = this.props;
+    let { planet, history } = this.props;
     if (!planet) { return <div> Loading... </div>}
     return(
       <div>
-        {this.props.planet.name}
-        <div>Residents</div>
-        <ul>
-          { this.displayResidents() }
-        </ul>
+        <Grid>
+          <Grid.Row centered>{this.props.planet.name}</Grid.Row>
+          <Grid.Row centered>Residents</Grid.Row>
+          <Grid.Row centered>
+            <List>{ this.displayResidents() }</List>
+          </Grid.Row>
+          <Grid.Row centered>
+            <Button primary onClick={ () => history.push('/people')}>
+              Back To People
+            </Button>
+          </Grid.Row>
+        </Grid>
+
       </div>
     )
   }
